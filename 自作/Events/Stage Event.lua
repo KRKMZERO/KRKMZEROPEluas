afa66002db2b13c67086d89cad55b4f378419564
黒髪零 <104833210+KRKMZERO@users.noreply.github.com>
2023-02-28 13:29:19 +0900

    Add files via upload

---
+++ 自作/Events/Stage Event.lua
@@ -0,0 +1,19 @@
+function onEvent(name, value1, value2)
+
+	if name == "Stage Event" then
+
+	end
+end
+
+--このEventLuaはハリボテです
+--stageのLuaに
+--function onEvent(name, value1, value2)
+--if name == "Stage Event" then
+--
+--対応した数字を入れるとよさそうです
+--if value1 == 2 then
+--
+--
+--
+--
+--
