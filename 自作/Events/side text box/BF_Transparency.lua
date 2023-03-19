2f76d2abf16767b721cf5c24f409760d75db3f4b
黒髪零 <104833210+KRKMZERO@users.noreply.github.com>
2023-03-19 22:19:01 +0900

    Delete 自作/Events/side text box directory

--- 自作/Events/side text box/BF_Transparency.lua
+++
@@ -1,6 +0,0 @@
-function onEvent(name,value1,value2)
-	if name == "BF_Transparency" then
-		doTweenAlpha('BFEvent1', 'boyfriend', tonumber(value1), tonumber(value2), 'linear')
-		doTweenAlpha('BFIEvent1', 'iconP1', tonumber(value1), tonumber(value2), 'linear')
-	end
-end
--- 自作/Events/side text box/DAD_Transparency.lua
+++
@@ -1,6 +0,0 @@
-function onEvent(name,value1,value2)
-	if name == "DAD_Transparency" then
-		doTweenAlpha('DADEvent1', 'dad', tonumber(value1), tonumber(value2), 'linear')
-		doTweenAlpha('DADIEvent1', 'iconP2', tonumber(value1), tonumber(value2), 'linear')
-	end
-end
